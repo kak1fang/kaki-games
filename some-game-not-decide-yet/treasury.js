c12e2cc44284cb3843c3668fc7d493759870518e
Your Name <you@example.com>
2018-11-02 10:35:27 -0400

    get image

--- some-game-not-decide-yet/treasury.js
+++ some-game-not-decide-yet/treasury.js
@@ -1,23 +1,23 @@
 
+var Ufo;
 
-var setup = function(){
 
+var preload = function() {
+    
+    Ufo = loadImage('assets/ufo_game_enemy.png');
+
+}
+
+function setup(){
     var canvas = createCanvas(400,400);
     canvas.parent("sketch-holder");
+
 }
-function preload(){
-    var img;
-    img('assets/ufo_game_enemy.png', 10,10, 50, 50);  
+
+var draw = function(){
+
+    imageMode(CENTER);
+    image(Ufo,200,200,100,100);
+
+
 }
-   
-
-var draw= function(){
-    background(50)
-    ellipse(200,200,100,100);
-    img('assets/ufo_game_enemy.png', 10,10, 50, 50);
-};
-
-var img;
-
-
-
