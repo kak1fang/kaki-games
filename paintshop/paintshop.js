11a0ed3b23de0dc650995ac699c6dbd6ccc8754a
Your Name <you@example.com>
2019-03-05 09:17:13 -0500

    paint shop

--- paintshop/paintshop.js
+++ paintshop/paintshop.js
@@ -2,19 +2,14 @@ function preload(){
 
 }
 function setup(){
-    let imgElement = createImg('https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/2000px-Flag_of_Vietnam.svg.png')
-    imgElement.attribute('height', "75")
+    createCanvas(windowWidth, windowHeight);
+    background(255, 255, 255);
 }
 function draw(){
-
+    
 }
-function boxWasClicked(event){
-
-    console.log("Box clicked", event.target);
-
-    let boxDiv = event.target;
-
-    boxDiv.style.backgroundColor = "rgb(random(255), random(255),random(255))"
-
-
-}
+//we dont want to erase the painting
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+//     background(0, 0, 50);
+// }
