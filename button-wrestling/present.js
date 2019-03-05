fa9adc442f3760ec0f37b62d30da7b7cc92e3cf1
Your Name <you@example.com>
2019-03-05 08:30:18 -0500

    update

--- button-wrestling/present.js
+++ button-wrestling/present.js
@@ -1,6 +1,8 @@
 var sketchProc = function(processingInstance) {
     with (processingInstance) {
        size(400,400)
-       
+       loadimage = "ges.jpg";
+       loadimage = "images.jpg";
+       loadimage = "imaes.jpg";
     }
 };
